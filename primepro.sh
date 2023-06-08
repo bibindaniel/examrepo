5529b5c0a8ba590578be57f23c8d01e01f62dc20
bibindaniel <bibindaniel10@gmail.com>
2023-06-08 11:44:49 +0530

    add files

---
+++ primepro.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -e "Enter number: \c"
+read num
+
+for ((i=2; i<=num/2; i++))
+do
+    if [ $((num % i)) -eq 0 ]; then
+        echo "$num is not a prime number"
+        exit 0
+    fi
+done
+
+echo "$num is a prime number"
